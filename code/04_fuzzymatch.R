cbea710bece88a65b4902c68627b403ed1ec282a
matthew p robertson <35159349+mpr1255@users.noreply.github.com>
2022-07-26 13:56:58 +1000

    minor updates to fuzzymatch for show and tell

--- code/04_fuzzymatch.R
+++ code/04_fuzzymatch.R
@@ -7,7 +7,7 @@ suppressMessages(library(furrr))
 plan(multisession, workers = 8)
 
 # Set up strings to match. ts = 'target strings'
-ts_intubation <- as_utf8(c("脑死亡后用麻醉机维持呼吸", "死亡后迅速建立人工呼吸", "自主呼吸丧失的脑死亡供体,在特定条件下应尽可能迅速建立辅助呼吸支持循环,维持供心的血氧供应,避免或缩短热缺血时间,同时迅速剖胸取心", "供体大脑死亡后,首先分秒必争地建立呼吸与静脉通道", "经气管切开气管插管建立人工呼吸", "快速胸部正中切口进胸", "供者脑死亡后迅速建立人工呼吸", "供心保护脑死亡后用麻醉机维持呼吸", "供体确定脑死亡后,气管插管,彻底吸除气道分泌物,用简易呼吸器人工控制呼吸", "供体脑死亡后,迅速建立人工呼吸", "供体脑死亡后快速正中开胸,同时插入气管导管人工通气", "脑死亡后,紧急气管插管", "供者行气管插管", "供者行气管插管,球囊加压通气,静脉注射肝素200mg", "脑死亡后，用麻醉机维持呼吸", "供体在确认脑死亡后,气管插管,建立人工呼吸", "脑死亡后气管紧急插管,纯氧通气", "供体死亡后行人工呼吸、循环支持", "脑死亡后,气管插管", "脑死亡后立即气管内插管给氧", "脑死亡,面罩加压给氧,辅助呼吸", "脑死亡后,将供体取仰卧位,争取做气管插管", "脑死亡后迅速气管插管", "脑死亡后迅速气管插管进行机械通气", "协助麻醉医生进行支纤镜检查后进行供体气管插管", "脑死亡后,4例气管插管,3例面罩吸氧", "脑死亡后插入气管导管", "在这紧急情况下,必须在紧急开胸的同时,进行紧急气管插管及辅助呼吸", "供体手术气管插管通气", "供体手术气管插管", "气管切开气管插管", "供体心脏的提取供心者取仰卧位,垫高胸腔,气管插管", "进行供心、肺切取,吸净气管分泌物,气管插管给氧", "供体心肺的切取气管插管", "供肺切取:供体气管插管", "供者平卧位,气管插管", "供心切取配合,护士协助医生气管插管辅助呼吸", "供心切取配合..气管插管", "供体平卧位，气管插管", "协助麻醉医生进行支纤镜检查后进行气管插管", "供体心肺的获取和保护..行气管插管通气", "供心的切取供体气管插管后", "供者气管插管", "供体全身肝素化后，仰卧位，经口气管内插管","面罩吸氧"))
+ts_intubation <- utf8::as_utf8(c("脑死亡后用麻醉机维持呼吸", "死亡后迅速建立人工呼吸", "自主呼吸丧失的脑死亡供体,在特定条件下应尽可能迅速建立辅助呼吸支持循环,维持供心的血氧供应,避免或缩短热缺血时间,同时迅速剖胸取心", "供体大脑死亡后,首先分秒必争地建立呼吸与静脉通道", "经气管切开气管插管建立人工呼吸", "快速胸部正中切口进胸", "供者脑死亡后迅速建立人工呼吸", "供心保护脑死亡后用麻醉机维持呼吸", "供体确定脑死亡后,气管插管,彻底吸除气道分泌物,用简易呼吸器人工控制呼吸", "供体脑死亡后,迅速建立人工呼吸", "供体脑死亡后快速正中开胸,同时插入气管导管人工通气", "脑死亡后,紧急气管插管", "供者行气管插管", "供者行气管插管,球囊加压通气,静脉注射肝素200mg", "脑死亡后，用麻醉机维持呼吸", "供体在确认脑死亡后,气管插管,建立人工呼吸", "脑死亡后气管紧急插管,纯氧通气", "供体死亡后行人工呼吸、循环支持", "脑死亡后,气管插管", "脑死亡后立即气管内插管给氧", "脑死亡,面罩加压给氧,辅助呼吸", "脑死亡后,将供体取仰卧位,争取做气管插管", "脑死亡后迅速气管插管", "脑死亡后迅速气管插管进行机械通气", "协助麻醉医生进行支纤镜检查后进行供体气管插管", "脑死亡后,4例气管插管,3例面罩吸氧", "脑死亡后插入气管导管", "在这紧急情况下,必须在紧急开胸的同时,进行紧急气管插管及辅助呼吸", "供体手术气管插管通气", "供体手术气管插管", "气管切开气管插管", "供体心脏的提取供心者取仰卧位,垫高胸腔,气管插管", "进行供心、肺切取,吸净气管分泌物,气管插管给氧", "供体心肺的切取气管插管", "供肺切取:供体气管插管", "供者平卧位,气管插管", "供心切取配合,护士协助医生气管插管辅助呼吸", "供心切取配合..气管插管", "供体平卧位，气管插管", "协助麻醉医生进行支纤镜检查后进行气管插管", "供体心肺的获取和保护..行气管插管通气", "供心的切取供体气管插管后", "供者气管插管", "供体全身肝素化后，仰卧位，经口气管内插管","面罩吸氧"))
 
 target_strings <- ts_intubation
 
@@ -15,14 +15,13 @@ target_strings <- ts_intubation
 target_strings %>% .[duplicated(.)]
 
 # Read in list of files
-path <- ("./data/papers_txt_clean/")
-file_list_w_path <- list.files(path, full.names = TRUE)
-file_list_no_path <- list.files(path)
-file_list <- as.data.table(cbind(file_list_w_path, file_list_no_path))
+path <- ("./data/txt/")
+file_list <- data.table(file_w_path = list.files(path, full.names = TRUE))[,basename := basename(file_w_path)]
 
 # Set up main functions
-get_string_matches <- function(file_text, target_string){
-  res <- afind(file_text, target_string, window = nchar(target_string), method="running_cosine")
+getStringMatches <- function(file_text, target_string){
+  # target_string <- target_strings[1]
+  res <- stringdist::afind(file_text, target_string, window = nchar(target_string), method="running_cosine")
   location <- res$location
   distance <- res$distance
   match <- res$match
@@ -31,16 +30,24 @@ get_string_matches <- function(file_text, target_string){
   return(res2)
 }
 
-get_full_match <- function(path, file_name, target_strings) {
-  file_text <- fread(paste0(path, file_name), sep = NULL, header = FALSE)
-  res_afind <- future_map(target_strings, ~get_string_matches(file_text, .x))
-  res <- rbindlist(res_afind)
-  res3 <- as.data.table(cbind(path, file_name, res))
-  names(res3) <- c("path", "file_name", "target_string", "string_location",  "string_distance", "matching_string", "context")
-  return(res3)
+getFullMatch <- function(file, target_strings) {
+  # file <- "./data/txt/0111.txt"
+  file_text <- fread(file, sep = NULL, header = FALSE)
+  res <- rbindlist(future_map(target_strings, ~getStringMatches(file_text, .x)))
+  res <- cbind(file, res)
+  names(res) <- c("file", "target_string", "string_location",  "string_distance", "matching_string", "context")
+  return(res)
 }
+possibly_getFullMatch <- possibly(getFullMatch, otherwise = NA)
 
-all_res <- future_map_dfr(file_list$file_list_no_path, ~get_full_match(path, .x, target_strings))
+all_res <- future_map_dfr(file_list$file_w_path, ~possibly_getFullMatch(.x, target_strings))
+
+# Show-and-tell -----------------------------------------------------------
+result <- possibly_getFullMatch("./data/txt/0111.txt", target_strings)
+result[string_distance < .20]
+
+setDT(result)
+
 
 # Previously output here.
 # all_res <- fread("./data/string_match_full_output.csv")
